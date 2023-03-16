a15ebac2d93b5fc8fb747860f01312ecffc5b719
Ben Adamsky <benadamsky@gmail.com>
2023-03-15 23:05:57 -0400

    multi line responses are okay

--- src/utils/validateResponse.ts
+++ src/utils/validateResponse.ts
@@ -1,5 +1,5 @@
 const validateResponse = (response: string) => {
-  const validator = new RegExp(/^([1-5])\W*(.*)$/)
+  const validator = new RegExp(/^([1-5])\W*(.*?)$/s)
   const match = response.match(validator)
   return match
 }
