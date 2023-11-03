4e85488734e12601bafc270bed581f9273aab2af
Ben Adamsky <benadamsky@gmail.com>
2023-11-03 16:05:09 -0400

    improve date tag

--- src/utils/getDateTag.ts
+++ src/utils/getDateTag.ts
@@ -1,10 +1,19 @@
 const getDateTag = () => {
   const today = new Date()
-  const formattedDate = `${today.getMonth() + 1}/${today.getDate()}/${today
-    .getFullYear()
-    .toString()}`
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: 'America/New_York', // Use EST for now
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  }
 
-  return `\n[${formattedDate}]`
+  const formattedDateTime = today.toLocaleString('en-US', options)
+
+  return `\n[${formattedDateTime} EST]`
 }
 
 export { getDateTag }
