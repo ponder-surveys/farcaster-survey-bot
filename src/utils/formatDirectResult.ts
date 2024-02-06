bdd3a1cb72c67561103e586506d69d23823ef16d
Ben Adamsky <benadamsky@gmail.com>
2024-02-05 23:18:03 -0500

    add missing @ on author handles

--- src/utils/formatDirectResult.ts
+++ src/utils/formatDirectResult.ts
@@ -1,6 +1,6 @@
 const formatDirectResult = (authorUsername: string | null) => {
   const top = `🌟 Well done! You've successfully answered the question ${
-    authorUsername ? `by ${authorUsername} ` : ''
+    authorUsername ? `by @${authorUsername} ` : ''
   }and your bounty has been unlocked.`
 
   const bottom = `Tap on "View bounty" to claim your rewards.`
@@ -9,7 +9,7 @@ const formatDirectResult = (authorUsername: string | null) => {
 
 const formatDirectQuestionFailed = (authorUsername: string | null) => {
   const top = `⌛️ Time's up! Unfortunately the question ${
-    authorUsername ? `by ${authorUsername} ` : ''
+    authorUsername ? `by @${authorUsername} ` : ''
   }was not answered in time.`
   const bottom = `The full bounty amount has been returned to the author.`
   return `${top}\n\n${bottom}`
