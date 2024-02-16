81a48d52e7ced7b64a744fc06d3cffa86b4177dc
Ben Adamsky <benadamsky@gmail.com>
2024-02-16 17:36:40 -0500

    add question format tweaks

--- src/utils/formatQuestion.ts
+++ src/utils/formatQuestion.ts
@@ -1,11 +1,9 @@
 const formatQuestion = (question: Question, username: string | null) => {
   const { inspired_by } = question
 
-  const by = username
-    ? `Question ${
-        inspired_by ? 'inspired ' : ''
-      }by @${username} - Live for 48 hours`
-    : ''
+  const by = `New question${username && inspired_by ? ' inspired' : ''}${
+    username ? ` by @${username}` : ''
+  } - Live for 48 hours`
 
   return `${by}\n\n${process.env.NEXT_QUESTION_INFO}`
 }
