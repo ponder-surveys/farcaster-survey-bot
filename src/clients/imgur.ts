1df915afc1216e20568e6c0fb492f27b70c5c816
Ben Adamsky <benadamsky@gmail.com>
2024-04-26 13:07:07 -0400

    kill imgur and openai gpt

--- src/clients/imgur.ts
+++
@@ -1,8 +0,0 @@
-import ImgurClient from 'imgur'
-
-const clientId = process.env.IMGUR_CLIENT_ID as string
-const clientSecret = process.env.IMGUR_CLIENT_SECRET as string
-const refreshToken = process.env.IMGUR_REFRESH_TOKEN as string
-const imgurClient = new ImgurClient({ clientId, clientSecret, refreshToken })
-
-export { imgurClient }
--- src/clients/openai.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from 'openai'
-
-const openAIApiKey = process.env.OPENAI_API_KEY as string
-const openAIClient = new OpenAI({
-  apiKey: openAIApiKey,
-})
-
-export { openAIClient }
