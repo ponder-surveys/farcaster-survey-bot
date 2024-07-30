141c8f5899973448053bd003093c5a3db955bfc1
Matthew Morek <matthew.morek@gmail.com>
2024-07-31 00:04:23 +0200

    🔧 Update survey frame URL and remove unused constant
    - Change `SURVEY_FRAME_URL` to use new API endpoint
    - Remove unused `FRAME_URL` constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,7 @@
 export const MAX_BYTE_SIZE = 320
-export const SURVEY_FRAME_URL = 'https://weponder.io/farcaster/surveys'
+export const SURVEY_FRAME_URL = 'https://frame.weponder.io/api/polls'
 export const CREATE_SURVEY_FRAME_URL =
   'https://weponder.io/api/surveys/frames/new'
-export const FRAME_URL = 'https://weponder.io/farcaster/frame'
 
 export const EDGE_FUNCTIONS_SERVER_URL =
   process.env.EDGE_FUNCTIONS_SERVER_URL || 'https://edgefunctions.weponder.io'
