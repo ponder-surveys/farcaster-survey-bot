6212f9f851669ff1beec4a92f8152650642ae387
erik <erik.tastepe@gmail.com>
2024-10-03 16:44:21 +0900

    added scheduling and db update logic

--- src/api/results.ts
+++ src/api/results.ts
@@ -3,6 +3,8 @@ import getErrorMessage from '../utils/getErrorMessage'
 import { getDateTag } from '../utils/getDateTag'
 import { endPoll } from '../services/endPoll'
 import { fetchBounty } from '../services/supabase'
+import { Poll } from 'types/polls'
+import { endPredictivePoll } from 'services/endPredictivePoll'
 
 const getNextResults = async (): Promise<Question[]> => {
   const timeInterval = Number(
@@ -57,4 +59,55 @@ const updateNextResult = async (questionId: number) => {
   console.log(`${getDateTag()} Question status successfully updated on db`)
 }
 
-export { getNextResults, updateNextResult }
+const getExpiredPredictivePolls = async (): Promise<Poll[]> => {
+  const now = Date.now()
+
+  const { data, error } = await supabaseClient
+    .from('questions')
+    .select('*')
+    .eq('status', 'posted')
+    .lte('expires_at', now)
+    .order('id', { ascending: true })
+
+  if (error) {
+    console.error(`${getDateTag()} ${error}`)
+    throw new Error(error.message)
+  }
+
+  return data
+}
+
+const updatePredictivePollResult = async (questionId: number) => {
+  const { data: poll, error } = await supabaseClient
+    .from('questions')
+    .update({
+      status: 'calculated',
+    })
+    .eq('id', questionId)
+    .select('*')
+    .single()
+
+  if (error) {
+    console.error(`${getDateTag()} ${error}`)
+    throw new Error(error.message)
+  }
+
+  const bounty = await fetchBounty(poll.bounty_id)
+
+  if (bounty.id && poll.status === 'calculated' && bounty.status === 'active') {
+    try {
+      await endPredictivePoll(poll, bounty)
+    } catch (error) {
+      throw new Error(getErrorMessage(error))
+    }
+  }
+
+  console.log(`${getDateTag()} Question status successfully updated on db`)
+}
+
+export {
+  getNextResults,
+  updateNextResult,
+  getExpiredPredictivePolls,
+  updatePredictivePollResult,
+}
