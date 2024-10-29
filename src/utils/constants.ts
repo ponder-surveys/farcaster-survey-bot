9f5da768af23bd22295841d8875dbfadb6d11744
Ben Adamsky <benadamsky@gmail.com>
2024-10-29 18:11:28 -0400

    add composer action predictive poll url to reward dc

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 export const MAX_BYTE_SIZE = 320
 export const SURVEY_FRAME_URL = 'https://frame.weponder.io/api/polls'
 export const QUESTION_FRAME_URL = 'https://frame.weponder.io/api/questions'
+export const COMPOSER_ACTION_PREDICTIVE_POLL_URL = `https://warpcast.com/~/composer-action?url=${encodeURIComponent(
+  `https://frame.weponder.io/api/actions/create-predict`
+)}`
 export const CREATE_SURVEY_FRAME_URL =
   'https://weponder.io/api/surveys/frames/new'
 
